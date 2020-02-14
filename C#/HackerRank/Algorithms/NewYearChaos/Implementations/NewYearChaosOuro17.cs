4f23fbac6a819da848df01331bfcdd0e87b091fa
Antonio Vidal <Antonio.Vidal@bestsecret.com>
2020-02-14 14:08:26 +0100

    Testing solution

--- C#/HackerRank/Algorithms/NewYearChaos/Implementations/NewYearChaosOuro17.cs
+++ C#/HackerRank/Algorithms/NewYearChaos/Implementations/NewYearChaosOuro17.cs
@@ -1,5 +1,5 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System;
+using System.IO;
 
 namespace HackerRank.Algorithms.NewYearChaos.Implementations
 {
@@ -20,37 +20,30 @@ namespace HackerRank.Algorithms.NewYearChaos.Implementations
 
             int bribes = 0;
 
-            while (!IsChaotic && i < q.Length)
+            while (!IsChaotic && i < (q.Length - 1))
             {
-                int current = q[i];
+                int currentTicket = q[i];
+                int nextTicket = q[i + 1];
+                int currentPos = (i + 1);
 
-                if ((current - i) > 3)
+                if ((currentTicket - i) > 3)
                 {
                     IsChaotic = true;
                 }
                 else
                 {
-                    if ((current - 1) <= i)
+                    // 1, 2, 5, 3, 7, 8, 6, 4
+                    if (currentTicket > nextTicket)
                     {
-                        bribes++;
+                        bribes += Math.Abs(currentTicket - currentPos);
                     }
 
                     i++;
                 }
             }
 
-            // Replace TextWriter with Console if you want to test on the web.
-            TextWriter.Write((IsChaotic) ? TooChaotic : bribes.ToString());
+            // Replace TextWriter with Console.WriteLine if you want to test on the web.
+            TextWriter.Write(IsChaotic ? TooChaotic : bribes.ToString());
         }
-
-        // 1 2 3 4 5 6 7 8
-
-        // 1 2 3 4 5 7 6 8
-        // 1 2 3 5 4 7 6 8
-        // 1 2 3 5 7 4 6 8
-        // 1 2 3 5 7 4 8 6
-        // 1 2 3 5 7 8 4 6
-        // 1 2 3 5 7 8 6 4
-        // 1 2 5 3 7 8 6 4 <= check diff by 2??
     }
 }
